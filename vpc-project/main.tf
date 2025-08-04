245e8d364a3329cbb380fef8e4ddd022e7af8366
Shreeraj Kolhe <shreeraj@lentra.ai>
2025-08-04 09:17:52 +0530

    Added vpc project files

--- vpc-project/main.tf
+++ vpc-project/main.tf
@@ -130,7 +130,6 @@ resource "aws_instance" "web" {
   vpc_security_group_ids = [aws_security_group.web-sg.id]
   subnet_id     = aws_subnet.public-subnet.id
   associate_public_ip_address = "true"
-  key_name               = "sk-demo"
   user_data     = <<-EOF
         #!/bin/bash
         sudo apt-get update -y
